569e4dca833374343f1bb4a4ace21998dd27713c
stevengalvis <steven.galvis@gmail.com>
2017-10-18 18:40:28 -0400

    added router for users

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "https://arbitrator-hamster-48873.netlify.com";
 
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || "mongodb://localhost/api/users";
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || "mongodb://localhost/movie-data";
--- users/router.js
+++ users/router.js
@@ -33,4 +33,86 @@ router.post("/", jsonParser, (req, res) => {
       location: nonStringField
     });
   }
+
+  // if the username and password arent trimmed we give an error
+  const explicitlyTrimmedFields = ["username", "password"];
+  const nonTrimmedField = explicitlyTrimmedFields.find(field => req.body[field].trim() !== req.body[field]);
+
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: "ValidationError",
+      message: "Cannot start or end with whitespace",
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 10,
+      max: 72
+    }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => "min" in sizedFields[field] && req.body[field].trim().length < sizedFields[field].min
+  );
+
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => "max" in sizedFields[field] && req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: "ValidationError",
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField].min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  let { username, password, firstName = "", lastName = "" } = req.body;
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+
+  return User.find({ username })
+    .count()
+    .then(count => {
+      if (count > 0) {
+        // there is an existing user with the same username
+        return Promise.reject({
+          code: 422,
+          reason: "ValidationError",
+          message: "Username already taken",
+          location: "username"
+        });
+      }
+      // if there is no existing user, hash the password
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash,
+        firstName,
+        lastName
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.apiRepr());
+    })
+    .catch(err => {
+      // forward validation erros on to the client
+      if (err.reason === "ValidationError") {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({ code: 500, message: "Internal server error" });
+    });
 });
+
+module.exports = { router };
